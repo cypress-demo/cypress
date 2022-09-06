667a9e676c3529587c20472033cdceca3e9de6c6
Yogesh Bheemaiah <yogeshbheemaiah@Yogeshs-MacBook-Air.local>
2022-09-06 16:39:59 -0400

    assertion demo tests and cypress version downgrade

---
+++ cypress/integration/api-request.spec.ts
@@ -0,0 +1,73 @@
+describe('api test suite', () => {
+
+    let sessionReqSpec: Partial<Cypress.RequestOptions>;
+    let fipFinderReqSpec: Partial<Cypress.RequestOptions>;
+    let planSearchReqSpec: Partial<Cypress.RequestOptions>;
+
+    before(() => {
+        
+        const baseUrl = 'https://p-gsg.digitaledge.cigna.com/igse/connecturedrx2';
+
+        sessionReqSpec = {
+            url: baseUrl,
+            method: 'GET',
+            failOnStatusCode: false,
+            qs: {
+                action: 'session-create'
+            }
+        }
+        
+        fipFinderReqSpec = {
+            url: baseUrl,
+            method: 'GET',
+            failOnStatusCode: false,
+            qs: {
+                action: 'fip-finder',
+                zip: 63043
+            }
+        }
+
+        planSearchReqSpec = {
+            url: baseUrl,
+            method: 'POST',
+            failOnStatusCode: false,
+            qs: {
+                action: 'plan-compare-search',
+                zip: 63043,
+                year: 2022,
+                planType: 7
+            }
+        }
+    })
+
+    it('simple api request call', () => {
+        cy.request(fipFinderReqSpec).then(apiResponse => {
+            console.log(apiResponse);
+            expect(apiResponse.status).to.be.eq(200);
+            expect(apiResponse.statusText).to.be.eq('OK');
+        })
+    })
+
+    it('nested api request call', () => {
+        
+        cy.request(fipFinderReqSpec).then(fipResponse => {
+            console.log(fipResponse);
+            expect(fipResponse.status).to.be.eq(200);
+            
+            cy.request(sessionReqSpec).then(sessionResponse => {
+                console.log(sessionResponse);
+                expect(fipResponse.status).to.be.eq(200);
+
+                planSearchReqSpec.qs['fips'] = fipResponse.body[0].CountyFIPS;
+                planSearchReqSpec.qs['sessionID'] = sessionResponse.body[0].SessionID;
+
+                cy.request(planSearchReqSpec).then(planSearchResponse => {
+                    console.log(planSearchResponse);
+                    expect(planSearchResponse.status).to.be.eq(200);
+                })
+            })
+
+        })
+        
+    })
+})
